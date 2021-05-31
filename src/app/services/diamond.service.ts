740e64da8049f8d3c53bcc2726c95e499d25725e
Skander <besbes.skander@gmail.com>
2021-05-31 15:41:54 +0200

    add spaces

--- src/app/services/diamond.service.ts
+++ src/app/services/diamond.service.ts
@@ -9,15 +9,31 @@ export class DiamondService {
   }
 
   displayDiamond(letter: string): string {
-    if(letter === 'A')
-    {
-      return 'A';
-    } else if (letter === 'B'){
-      return ' A \nB B\n A '
+  const size = letter.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+
+    if (letter === 'A') {
+      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A');
+    } else if (letter === 'B') {
+      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A') + '\n'
+        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
+        + this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A')
     } else if (letter === 'C') {
-      return '  A  \n B B \nC   C\n B B \n  A  ';
+      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A') + '\n'
+        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
+        + this.addSpaces(size, 'C') + 'C   C' + this.addSpaces(size, 'C') + '\n'
+        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
+        + this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A')
     } else {
       return letter;
     }
+  }
+
+  addSpaces(size: number, letter: string): string {
+    const numSpaces = size - (letter.charCodeAt(0) - 'A'.charCodeAt(0)) - 1;
+    let spaces = '';
+    for (let i = 0; i < numSpaces; i++) {
+      spaces += ' ';
+    }
+    return spaces;
   }
 }
