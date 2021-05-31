f994f1a61e4f4cbd0e11a0767937a4c4a61f53a6
Skander <besbes.skander@gmail.com>
2021-05-31 18:31:15 +0200

    Refactor

--- src/app/services/diamond.service.ts
+++ src/app/services/diamond.service.ts
@@ -12,54 +12,19 @@ export class DiamondService {
     const letterRank = letter.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
     if (letter === 'A') {
       return this.addSpaces(0) + 'A' + this.addSpaces(0);
-    } else if (letter === 'B') {
-      return this.addSpaces(1) + 'A' + this.addSpaces(1) + '\n'
-        + this.addSpaces(0) + 'B B' + this.addSpaces(0) + '\n'
-        + this.addSpaces(1) + 'A' + this.addSpaces(1)
-    } else if (letter === 'C') {
-      let result = '';
-      let depth = this.getDepth('A');
-      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('B');
-      result += this.addSpaces(letterRank - depth - 1) + 'B' + this.addSpaces(2 * depth - 1) + 'B' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('C');
-      result += this.addSpaces(letterRank - depth - 1) + 'C' + this.addSpaces(2 * depth - 1) + 'C' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('B');
-      result += this.addSpaces(letterRank - depth - 1) + 'B' + this.addSpaces(2 * depth - 1) + 'B' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('A');
-      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1);
-
-      return result;
-    } else if (letter === 'D') {
-      let result = '';
-      let depth = this.getDepth('A');
-      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('B');
-      result += this.addSpaces(letterRank - depth - 1) + 'B' + this.addSpaces(2 * depth - 1) + 'B' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('C');
-      result += this.addSpaces(letterRank - depth - 1) + 'C' + this.addSpaces(2 * depth - 1) + 'C' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('D');
-      result += this.addSpaces(letterRank - depth - 1) + 'D' + this.addSpaces(2 * depth - 1) + 'D' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('C');
-      result += this.addSpaces(letterRank - depth - 1) + 'C' + this.addSpaces(2 * depth - 1) + 'C' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('B');
-      result += this.addSpaces(letterRank - depth - 1) + 'B' + this.addSpaces(2 * depth - 1) + 'B' + this.addSpaces(letterRank - depth - 1) + '\n';
-
-      depth = this.getDepth('A');
-      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1);
-
-      return result;
     } else {
-      return letter;
+      let result = '';
+      let depth = this.getDepth('A');
+      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1) + '\n';
+
+      const diamondInnerSize = Math.floor(((letterRank * 2) - 3) / 2);
+      for (let i = diamondInnerSize; i >= -diamondInnerSize; i--) {
+        result += this.createLine(letterRank, i);
+      }
+      depth = this.getDepth('A');
+      result += this.addSpaces(letterRank - depth - 1) + 'A' + this.addSpaces(letterRank - depth - 1);
+
+      return result;
     }
   }
 
@@ -73,5 +38,15 @@ export class DiamondService {
 
   getDepth(letter: string): number {
     return letter.charCodeAt(0) - 'A'.charCodeAt(0);
+  }
+
+  getCurrentLetter(letterRank: number, index: number): string {
+    return String.fromCharCode('A'.charCodeAt(0) + (letterRank - Math.abs(index) - 1));
+  }
+
+  createLine(letterRank: number, i: number): string {
+    const currentLetter = this.getCurrentLetter(letterRank, i);
+    const depth = this.getDepth(currentLetter);
+    return this.addSpaces(letterRank - depth - 1) + currentLetter + this.addSpaces(2 * depth - 1) + currentLetter + this.addSpaces(letterRank - depth - 1) + '\n';
   }
 }
