6bdc7215016ab08965d636aa9758b972648cb005
Skander <besbes.skander@gmail.com>
2021-05-31 16:04:56 +0200

    Refactor

--- src/app/services/diamond.service.ts
+++ src/app/services/diamond.service.ts
@@ -12,24 +12,30 @@ export class DiamondService {
   const size = letter.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
 
     if (letter === 'A') {
-      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A');
+      return this.addSpaces(0) + 'A' + this.addSpaces(0);
     } else if (letter === 'B') {
-      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A') + '\n'
-        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
-        + this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A')
+      return this.addSpaces(1) + 'A' + this.addSpaces(1) + '\n'
+        + this.addSpaces(0) + 'B B' + this.addSpaces(0) + '\n'
+        + this.addSpaces(1) + 'A' + this.addSpaces(1)
     } else if (letter === 'C') {
-      return this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A') + '\n'
-        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
-        + this.addSpaces(size, 'C') + 'C   C' + this.addSpaces(size, 'C') + '\n'
-        + this.addSpaces(size, 'B') + 'B B' + this.addSpaces(size, 'B') + '\n'
-        + this.addSpaces(size, 'A') + 'A' + this.addSpaces(size, 'A')
+      let result = '';
+      let depth = ('A'.charCodeAt(0) - 'A'.charCodeAt(0));
+      result +=  this.addSpaces(size - depth - 1) + 'A' + this.addSpaces(size - depth - 1) + '\n';
+      depth = ('B'.charCodeAt(0) - 'A'.charCodeAt(0));
+      result +=  this.addSpaces(size - depth - 1) + 'B B' + this.addSpaces(size - depth - 1) + '\n';
+      depth = ('C'.charCodeAt(0) - 'A'.charCodeAt(0));
+      result +=  this.addSpaces(size - depth - 1) + 'C   C' + this.addSpaces(size - depth - 1) + '\n';
+      depth = ('B'.charCodeAt(0) - 'A'.charCodeAt(0));
+      result +=  this.addSpaces(size - depth - 1) + 'B B' + this.addSpaces(size - depth - 1) + '\n';
+      depth = ('A'.charCodeAt(0) - 'A'.charCodeAt(0));
+      result +=  this.addSpaces(size - depth - 1) + 'A' + this.addSpaces(size - depth - 1);
+      return result;
     } else {
       return letter;
     }
   }
 
-  addSpaces(size: number, letter: string): string {
-    const numSpaces = size - (letter.charCodeAt(0) - 'A'.charCodeAt(0)) - 1;
+  addSpaces(numSpaces: number): string {
     let spaces = '';
     for (let i = 0; i < numSpaces; i++) {
       spaces += ' ';
